4341c7091ad92ae9a5cebde83586c004fe8dca27
Michael Brewer <mb166@students.uwf.edu>
2023-02-24 17:22:35 -0600

    Python script will convert times from soc-sign-bitcoinotc.csv to human readable times. This output has been saved as bitcoinotcTimeStamps.csv

---
+++ wrangling.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+#adding labels to the columns since the csv does not have a header
+columnLabels = ['source node', 'target node', 'rating', 'time']
+
+#reading in csv from our chosen dataset
+df = pd.read_csv('data\soc-sign-bitcoinotc.csv', header=None, names=columnLabels)
+
+#converting to human readable times
+df['time'] = df['time'].map(lambda time: pd.to_datetime(time, unit='s'))
+
+#saving as new csv
+#df.to_csv('data\\bitcoinotcTimeStamps.csv')
+
+#printing out the beginning and end of the data
+head = df.head(50)
+tail = df.tail(50)
+print(head)
+print(tail)
