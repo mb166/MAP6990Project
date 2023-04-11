2791cd765622e6cf994aba4722c4890c3014aebe
Michael Brewer <mb166@students.uwf.edu>
2023-04-11 16:16:15 -0500

    Refactored wrangling.py and changed time conversion to iso8601 to be complaint with gephi

--- wrangling.py
+++ wrangling.py
@@ -1,19 +1,60 @@
 import pandas as pd
+import numpy as np
 
-#adding labels to the columns since the csv does not have a header
-columnLabels = ['source node', 'target node', 'rating', 'time']
+#converting to iso8601 date format for compatibility with gephi
+def convertTimeStamps(df):
+    #df['time'] = df['time'].map(lambda time: pd.to_datetime(time, unit='s'))
+    time = pd.to_datetime(df['time'], unit='s').dt.strftime('%Y-%m-%dT%H:%M:%SZ')
+    df['timeset'] = time
+    df = df.drop('time', axis=1)
+    return df
 
 #reading in csv from our chosen dataset
-df = pd.read_csv('data\soc-sign-bitcoinotc.csv', header=None, names=columnLabels)
+def readCSV(file):
+    return pd.read_csv(file, header=0)
 
-#converting to human readable times
-df['time'] = df['time'].map(lambda time: pd.to_datetime(time, unit='s'))
+#save to csv file
+def saveCSV(df, file, header=False):
+    df.to_csv(file, header = header, index=False)
 
-#saving as new csv
-#df.to_csv('data\\bitcoinotcTimeStamps.csv')
+#print some data from dataframe
+def printDF(df):
+    head = df.head(10)
+    tail = df.tail(10)
+    print("FIRST 10 ENTRIES\n", head)
+    print("LAST 10 ENTRIES \n", tail)
 
-#printing out the beginning and end of the data
-head = df.head(10)
-tail = df.tail(10)
-print("FIRST 10 ENTRIES\n", head)
-print("LAST 10 ENTRIES \n", tail)
+#finding the unique node ids used
+def findUniqueNodes(df):
+    #getting nodes from source and target columns
+    listOfNodes = df['Source'].unique()
+    listOfTargetNodes = df['Target'].unique()
+    listOfNodes.sort()
+    #merging source and target columns
+    listOfAllNodes = np.concatenate((listOfNodes, listOfTargetNodes), axis=None)
+    print(listOfNodes.shape)
+    print(listOfTargetNodes.shape)
+    nodeDF = pd.DataFrame(data = listOfAllNodes)
+    print(nodeDF)
+    #finding unique nodes from the list of all nodes
+    nodes = nodeDF[0].unique()
+    print(nodes.shape)
+    nodes.sort()
+    print(nodes)
+    uniqueNodeDF = pd.DataFrame(data = nodes, columns=['node'])
+    uniqueNodeDF['id'] = uniqueNodeDF.loc[:, 'node']
+    print(uniqueNodeDF.head(10))
+    return uniqueNodeDF
+
+
+#main
+originalDF = readCSV('data\soc-sign-bitcoinotc.csv')
+convertDF = convertTimeStamps(originalDF)
+saveCSV(convertDF, 'data\\bitcoinotcTimeStampsiso8601.csv', header=True)
+#df = readCSV('data\\bitcoinotcTimeStamps.csv')
+#printDF(df)
+#uniqueNodeDF = findUniqueNodes(df)
+#printDF(uniqueNodeDF)
+#saveCSV(uniqueNodeDF, 'data\\bitcoinotcNodes.csv', ['id', 'node'])
+
+
